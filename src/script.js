57dd3eeadf0b40c7708663133698e9a7abaf2d3f
darsi-ai <dariia.s707@gmail.com>
2022-10-15 21:21:56 +0300

    added some imrovements in js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
+let time = new Date();
+let days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+let day = days[time.getDay()];
+let hour = time.getHours();
+let minutes = time.getMinutes();
+if (minutes < 10){
+    minutes = "0"+minutes;
+}
+
+let dayTime = document.querySelector("#dayTime");
+dayTime.innerHTML = `${day} ${hour}:${minutes}`;
+
 function displayTemparature(response){
     console.log(response);
-    //let desc = document.querySelector("#description");
-    //desc.innerHTML = `${response.data.weather[description]}`;
+    let cityName = document.querySelector("#city");
+    cityName.innerHTML = `${response.data.name}`
+    let desc = document.querySelector("#description");
+    weatherVal = response.data.weather[0];
+    desc.innerHTML = `${weatherVal.description}`;
     let deg = document.querySelector("#degrees");
     deg.innerHTML = `${Math.round(response.data.main.temp)}°`;
+    let feelLike = document.querySelector("#feelsLike");
+    feelLike.innerHTML = `feels like: ${Math.round(response.data.main.feels_like)}°C`
     let hum = document.querySelector("#humidity");
     hum.innerHTML = `humidity: ${response.data.main.humidity}%`;
     let windVal = document.querySelector("#wind");
